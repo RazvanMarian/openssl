55d61f57552f0c6ad23a69b1f740681a44214aca
RazvanMarian <marianrazvan84@yahoo.com>
2022-02-26 18:18:25 +0200

    Implemented signing and verifying for schnorr command line

--- apps/schnorr.c
+++ apps/schnorr.c
@@ -8,20 +8,10 @@
  */
 
 #include <openssl/opensslconf.h>
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include "apps.h"
 #include "progs.h"
 #include <openssl/bio.h>
-#include <openssl/err.h>
-#include <openssl/dsa.h>
-#include <openssl/evp.h>
-#include <openssl/x509.h>
-#include <openssl/pem.h>
-#include <openssl/bn.h>
 #include <openssl/schnorr.h>
 
 typedef enum OPTION_choice
@@ -62,7 +52,7 @@ int schnorr_main(int argc, char **argv)
     char *infile = NULL, *outfile = NULL, *prog;
     OPTION_CHOICE o;
     int text = 0, noout = 0;
-    int i, pubin = 0, pubout = 0, ret = 1;
+    int pubin = 0, pubout = 0, ret = 1;
     int sign = 0, verify = 0, generate = 0;
     char *keyfile = NULL, *sigfile = NULL;
 
@@ -107,7 +97,7 @@ int schnorr_main(int argc, char **argv)
             sigfile = opt_arg();
             break;
         case OPT_VERIFY:
-            verify = 1;
+            pubin = verify = 1;
             keyfile = opt_arg();
             break;
         case OPT_GENERATE:
@@ -117,6 +107,7 @@ int schnorr_main(int argc, char **argv)
     }
 
     argc = opt_num_rest();
+    argv = opt_rest();
     if (argc > 1)
         goto opthelp;
 
@@ -124,7 +115,6 @@ int schnorr_main(int argc, char **argv)
     {
         if (outfile != NULL)
         {
-            printf("outputfile : %s\n", outfile);
             EC_KEY *key;
             int ret_code = Gen(&key);
             if (ret_code != 0)
@@ -147,12 +137,38 @@ int schnorr_main(int argc, char **argv)
         goto end;
     }
 
+    if (pubout)
+    {
+        if (infile != NULL && outfile != NULL)
+        {
+            EC_KEY *key_pair = EC_KEY_new();
+            if (Read_Schnorr_Private_key(&key_pair, infile) != 0)
+            {
+                BIO_printf(bio_err, "%s: Could not open the file to be signed.\n", prog);
+                goto end;
+            }
+
+            if (Write_Schnorr_Public_Key(key_pair, outfile) != 0)
+            {
+                BIO_printf(bio_err, "%s: Could not open the file to be signed.\n", prog);
+                goto end;
+            }
+
+            printf("Public key written to %s.\n", outfile);
+        }
+        else
+        {
+            goto opthelp;
+        }
+    }
+
     if (text)
     {
         BIO *out = BIO_new_fp(stdout, 0);
         EVP_PKEY *pk = EVP_PKEY_new();
         pk = load_key(infile, FORMAT_PEM, 1, NULL, NULL, "Private Key");
         EVP_PKEY_print_private(out, pk, 0, NULL);
+        BIO_free_all(out);
     }
 
     if (noout)
@@ -161,6 +177,126 @@ int schnorr_main(int argc, char **argv)
         goto end;
     }
 
+    if (sign)
+    {
+        if (argv[0] != NULL && keyfile != NULL && outfile != NULL)
+        {
+            char *file_to_be_signed = argv[0];
+
+            // read file input
+            FILE *f_in = fopen(file_to_be_signed, "rb");
+            if (f_in == NULL)
+            {
+                BIO_printf(bio_err, "%s: Could not open the file to be signed.\n", prog);
+                goto end;
+            }
+            fseek(f_in, 0, SEEK_END);
+            long fsize = ftell(f_in);
+            fseek(f_in, 0, SEEK_SET); /* same as rewind(f); */
+
+            char *file_content = malloc(fsize + 1);
+            if (fread(file_content, fsize, 1, f_in) == 0)
+            {
+                BIO_printf(bio_err, "%s: Could not read from the file to be signed.\n", prog);
+                goto end;
+            }
+            fclose(f_in);
+            file_content[fsize] = 0;
+            //*******************************
+
+            // Compute file hash
+            unsigned char *file_hash = (unsigned char *)malloc(SHA256_DIGEST_LENGTH);
+            SHA256((unsigned char *)file_content, fsize, file_hash);
+
+            // load private key
+
+            EC_KEY *private_key = EC_KEY_new();
+            if (Read_Schnorr_Private_key(&private_key, keyfile) != 0)
+            {
+                BIO_printf(bio_err, "%s: Could not load the private key.\n", prog);
+                goto end;
+            }
+
+            // sign the document
+            schnorr_signature *signature = Schnorr_SIG_new();
+
+            if (Schnorr_Sign(private_key, (const char *)file_hash, SHA256_DIGEST_LENGTH, signature) != 0)
+            {
+                BIO_printf(bio_err, "%s: Error signing the file.\n", prog);
+                goto end;
+            }
+
+            if (Write_Schnorr_Signature(signature, outfile) != 0)
+            {
+                BIO_printf(bio_err, "%s: Error writing the signature in the output file.\n", prog);
+                goto end;
+            }
+
+            printf("The file %s was signed. The signature should be in %s.\n", file_to_be_signed, outfile);
+        }
+        else
+        {
+            goto opthelp;
+        }
+    }
+
+    if (verify)
+    {
+        if (pubin && sigfile != NULL && argv[0] != NULL)
+        {
+            char *file_to_be_verified = argv[0];
+
+            // read file input
+            FILE *f_in = fopen(file_to_be_verified, "rb");
+            if (f_in == NULL)
+            {
+                BIO_printf(bio_err, "%s: Could not open the file to be signed.\n", prog);
+                goto end;
+            }
+            fseek(f_in, 0, SEEK_END);
+            long fsize = ftell(f_in);
+            fseek(f_in, 0, SEEK_SET); /* same as rewind(f); */
+
+            char *file_content = malloc(fsize + 1);
+            if (fread(file_content, fsize, 1, f_in) == 0)
+            {
+                BIO_printf(bio_err, "%s: Could not read from the file to be signed.\n", prog);
+                goto end;
+            }
+            fclose(f_in);
+            file_content[fsize] = 0;
+            //*******************************
+
+            // Compute file hash
+            unsigned char *file_hash = (unsigned char *)malloc(SHA256_DIGEST_LENGTH);
+            SHA256((unsigned char *)file_content, fsize, file_hash);
+
+            EC_KEY *public_key = EC_KEY_new();
+            if (Read_Schnorr_Public_Key(&public_key, keyfile) != 0)
+            {
+                BIO_printf(bio_err, "%s: Could not load the private key.\n", prog);
+                goto end;
+            }
+
+            schnorr_signature *signature = Schnorr_SIG_new();
+            if (Read_Schnorr_Signature(signature, sigfile) != 0)
+            {
+                BIO_printf(bio_err, "%s: Could not read the signature from the specified file.\n", prog);
+                goto end;
+            }
+
+            if (Verify_Sign(public_key, (const char *)file_hash, SHA256_DIGEST_LENGTH, signature) != 0)
+            {
+                BIO_printf(bio_err, "%s: Could not verify the signature. Signature IS NOT OK.\n", prog);
+                goto end;
+            }
+            printf("Verified OK!\n");
+        }
+        else
+        {
+            goto opthelp;
+        }
+    }
     ret = 0;
 end:
     return ret;
